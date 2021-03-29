f0971a5151945bbd3764eee52fd07bbc7855c1f5
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-03-29 04:02:10 -0400

    Added about page and fixed a few things

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Typography, Grid, IconButton } from "@material-ui/core";
-
+import { Typography } from "@material-ui/core";
 class About extends React.Component {
   render() {
     return (
@@ -15,6 +14,22 @@ class About extends React.Component {
         >
           About Doodle Recognizer
         </Typography>
+
+        <section
+          className="section"
+          style={{ textAlign: "start", margin: "20px" }}
+        >
+          <div className="box">
+            <div className="content is-normal has-text-primary-light">
+              <h2 className="has-text-danger">Algorithms Supported</h2>
+              <p>
+                The doodle recognizer project uses two algorithms neural
+                networks convolution and OpenCV patch based template matching to
+                recognize the doodle drawn by the user.
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
